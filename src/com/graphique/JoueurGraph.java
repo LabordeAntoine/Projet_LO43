52c0603cb101d81e76857ccb9963bc08dffb2492
Antoine <antoine71880@hotmail.fr>
2018-12-15 18:07:25 +0100

    legere modife

--- src/com/graphique/JoueurGraph.java
+++ src/com/graphique/JoueurGraph.java
@@ -11,36 +11,23 @@ import java.awt.event.ActionListener;
 
 public class JoueurGraph extends JPanel {
 
-    public JoueurGraph(Joueur j, Color Couleur){
+    public JoueurGraph(Joueur j, Color couleur){
         EmptyBorder marge = new EmptyBorder(10,10,10,10);
 
         this.setLayout(new GridLayout(2,1));
         this.setSize(new Dimension(60,20));
         this.setBorder(BorderFactory.createMatteBorder(1,1,1,1, Color.black));
         this.setBounds(0,0,600,400);
-        this.setOpaque(false);
         this.setPreferredSize(new Dimension(250,250));
         JLabel label = new JLabel();
         label.setText(j.getName());
-        label.setForeground(Couleur);
+        label.setForeground(couleur);
         JTextArea ressource = new JTextArea();
         ressource.setBorder(marge);
         ressource.setEditable(false);
         ressource.setText(j.toStringRessources());
 
-        JButton bouton = new JButton("Test");
-        bouton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                j.ajouterRessources(Ressources.BLE);
-                ressource.setText(j.toStringRessources());
-            }
-        });
-
-
         this.add(label);
         this.add(ressource);
-        this.add(bouton);
     }
-
-
 }
