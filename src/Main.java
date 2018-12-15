a3fa42946c31f8ff768730d7af2d51de60fd4d0e
Antoine <antoine71880@hotmail.fr>
2018-12-15 18:10:24 +0100

    legere modife

--- src/Main.java
+++ src/Main.java
@@ -8,40 +8,10 @@ import com.graphique.Fenetre;
 public class Main {
     public static void main(String[] args) {
 
-
-        Joueur j = new Joueur("Antoine");
-        Joueur j1 = new Joueur("Jean");
-
-        j.ajouterRessources(Ressources.BLE);
-        j.ajouterRessources(Ressources.BLE);
-        j.ajouterRessources(Ressources.BLE);
-        j.ajouterRessources(Ressources.BLE);
-        j.ajouterRessources(Ressources.BLE);
-        j.ajouterRessources(Ressources.BLE);
-
-        j.ajouterRessources(Ressources.BOIS);
-        j.ajouterRessources(Ressources.BOIS);
-        j.ajouterRessources(Ressources.BOIS);
-
-        j.ajouterRessources(Ressources.FER);
-        j.ajouterRessources(Ressources.FER);
-        j.ajouterRessources(com.modele.ressources.Ressources.FER);
-
-        j1.ajouterRessources(Ressources.BOIS);
-        j1.ajouterRessources(Ressources.BOIS);
-        j1.ajouterRessources(Ressources.BOIS);
-
-        j.afficherRessource();
-
-
-        System.out.println(" ------------------------------");
         FenetreLancement F = new FenetreLancement();
         F.setVisible(true);
-
-
+        
         System.out.println(" ------------------------------");
-
-
 
         GameController gc = new GameController();
         System.out.println(gc.toString());
