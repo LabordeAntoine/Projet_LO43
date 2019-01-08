0ed4776701f089108b6560b5d90ff58824dc2649
antoine <antoine71880@hotmail.fr>
2019-01-08 09:25:32 +0100

    Ajout fenêtre commerce

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,10 @@
 import com.graphique.fenetre_lancement.FenetreLancement;
-import com.graphique.fenetre_principale.FenetrePrincipale;
-import com.graphique.fenetre_principale.plateau.PlateauException;
-import com.modele.GameController;
-import com.modele.joueur.Joueur;
-
-import java.awt.*;
-import java.awt.geom.Point2D;
-import java.awt.geom.Point2D.Double;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
         FenetreLancement f = new FenetreLancement();
         f.setVisible(true);
-
-
-        //new GameController();
-        //System.out.println(j1.getListeRessources().toString());
-    	/*ArrayList<Joueur> listeJoueur = new ArrayList<>();
-        Joueur j1 = new Joueur("Antoine ", Color.MAGENTA);
-        Joueur j2 = new Joueur("Ewan", Color.BLUE);
-        Joueur j3 = new Joueur("Yosef", Color.RED);
-        Joueur j4 = new Joueur("TEST", Color.GREEN);
-        listeJoueur.add(j1);
-        listeJoueur.add(j2);
-        listeJoueur.add(j3);
-        listeJoueur.add(j4);
-        
-        try {
-            new FenetrePrincipale(listeJoueur);
-        } catch (PlateauException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(" ------------------------------");*/
-
-        /*GameController gc = new GameController();
-        System.out.println(gc.toString());*/
 
 
     }
--- src/com/graphique/fenetre_principale/Commerce.java
+++ src/com/graphique/fenetre_principale/Commerce.java
@@ -1,27 +1,32 @@
 package com.graphique.fenetre_principale;
 
 import com.modele.joueur.Joueur;
-
 import javax.swing.*;
 
 public class Commerce extends JFrame {
+
 
     public Commerce(Joueur j){
         this.setTitle("Commerce");
         this.setSize(300, 300);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
+        RessourcesDispoEchange(j);
         this.setVisible(true);
 
-        RessourcesDispoEchange(j);
     }
 
     private void RessourcesDispoEchange(Joueur j){
         JPanel panRessourceJ = new JPanel();
-        JTextArea textRessource = new JTextArea();
-
+        JTextArea textRessource = new JTextArea("test");
+        textRessource.setEditable(false);
         textRessource.setText(j.toStringRessources());
+
         panRessourceJ.add(textRessource);
+        JButton echangerBanque = new JButton("Echanger (ne fait rien)");
+        panRessourceJ.add(echangerBanque);
         this.add(panRessourceJ);
+
     }
+
 }
